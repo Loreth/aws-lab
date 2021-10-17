e222842f9683467372b0ee3843a30604cf4d8467
Urszula Raczycka <49002596+stillnoidea@users.noreply.github.com>
2021-10-18 00:15:13 +0200

    deleted 3 imports with same values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatButtonModule} from "@angular/material/button";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
-import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
