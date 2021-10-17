e4f16535a48cffa9350a99a89d03e94252863182
Urszula Mazur <ulam98@gmail.com>
2021-10-17 18:36:20 +0200

    Added components for archiving files in buckets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
 import {MatTableModule} from "@angular/material/table";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatRippleModule} from "@angular/material/core";
+import { BucketArchiveComponent } from './components/bucket/bucket-archive/bucket-archive.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {MatRippleModule} from "@angular/material/core";
     AdminPanelComponent,
     HeaderComponent,
     BucketBrowserComponent,
-    BucketPickerComponent
+    BucketPickerComponent,
+    BucketArchiveComponent
   ],
     imports: [
         BrowserModule,
---
+++ src/app/components/bucket/bucket-archive/bucket-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bucket-archive',
+  templateUrl: './bucket-archive.component.html',
+  styleUrls: ['./bucket-archive.component.css']
+})
+export class BucketArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
