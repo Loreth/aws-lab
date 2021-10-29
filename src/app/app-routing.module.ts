68acb75bae909afb2adb714b77f370097ccd0996
Kamil Przeniosło <kamillo.prz@gmail.com>
2021-10-30 00:45:54 +0200

    Add redirection for 404 paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,8 @@ const routes: Routes = [
       {path: 'rekognition-labeling', component: RekognitionLabelingComponent},
       {path: 'textract-ocr', component: TextractOcrComponent}
     ]
-  }
+  },
+  {path: '**', redirectTo: ''},
 ]
 
 @NgModule({
